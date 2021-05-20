879d0bd945620e54ed65488f72dd820705ef6e9f
fabiocardosolisboa <fabio.cardosolisboa7@gmail.com>
2021-05-20 15:58:48 -0300

    usando lista com fotos,listview,propriedades do listview

---
+++ TestandoFotos/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace TestandoFotos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imageList = new System.Windows.Forms.ListView();
+            this.selectedImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.selectedImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList
+            // 
+            this.imageList.HideSelection = false;
+            this.imageList.Location = new System.Drawing.Point(12, 12);
+            this.imageList.Name = "imageList";
+            this.imageList.Size = new System.Drawing.Size(207, 335);
+            this.imageList.TabIndex = 0;
+            this.imageList.UseCompatibleStateImageBehavior = false;
+            this.imageList.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.imageList_ItemSelectionChanged);
+            // 
+            // selectedImage
+            // 
+            this.selectedImage.Location = new System.Drawing.Point(307, 12);
+            this.selectedImage.Name = "selectedImage";
+            this.selectedImage.Size = new System.Drawing.Size(431, 335);
+            this.selectedImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.selectedImage.TabIndex = 1;
+            this.selectedImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 359);
+            this.Controls.Add(this.selectedImage);
+            this.Controls.Add(this.imageList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.selectedImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView imageList;
+        private System.Windows.Forms.PictureBox selectedImage;
+    }
+}
+
---
+++ TestandoFotos/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TestandoFotos
+{
+    public partial class Form1 : Form
+    {
+        private List<Image> LoadedImages { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //carregando imagens da pasta
+            LoadImagesFromFolder();
+
+            //inicializando lista de imagens
+            ImageList images = new ImageList();
+            images.ImageSize = new Size(130,40);
+
+            foreach(var image in LoadedImages) 
+            {
+                images.Images.Add(image);
+            }
+
+            //configurando nossa listview com imageList
+            imageList.LargeImageList = images;
+
+            for(int itemIndex = 1; itemIndex <= LoadedImages.Count; itemIndex++) 
+            {
+                imageList.Items.Add(new ListViewItem($"Imagem {itemIndex}", itemIndex-1));
+            }
+        }
+        private void LoadImagesFromFolder() 
+        {
+            LoadedImages = new List<Image>();
+            var index = 1;
+            while (index < 4) 
+            {
+                string tempLocation = $@"C:\Users\fabio\Desktop\imagens\{index}.jpg";
+                var tempImage = Image.FromFile(tempLocation);
+                LoadedImages.Add(tempImage);
+                index += 1;
+            }
+        }
+
+        private void imageList_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            if (imageList.SelectedIndices.Count > 0) 
+            {
+                var selectedIndex = imageList.SelectedIndices[0];
+                Image selectedImg = LoadedImages[selectedIndex];
+                selectedImage.Image = selectedImg;
+            }
+        }
+    }
+}
